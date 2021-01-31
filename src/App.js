9521b9b8904b691811e25d82b799fe66f18397ad
WooSangGyu <wsg0701@naver.com>
2021-01-31 13:51:05 +0900

    Use Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
-import Potato from "./Potato";
+
+// 첫번째 인자로 props를 받는다 -> { fav } == props.fav
+function Food({ fav }) {
+  return <h1>I like {fav}</h1>;
+}
 
 function App() {
   return (
     <div>
       <h1>Hello!!</h1>
-      <Potato />
+      <Food fav="Kimchi" />
+      <Food fav="ramen" />
+      <Food fav="samgiopsal" />
+      <Food fav="chukumi" />
     </div>
   );
 }
