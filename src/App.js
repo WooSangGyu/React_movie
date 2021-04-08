58402757c020ea1d7a7b3bb7ca204839a0162dc3
WooSangGyu <wsg0701@naver.com>
2021-04-08 14:29:25 +0900

    Add axios and fetching movies from API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
   state = {
@@ -6,10 +7,13 @@ class App extends React.Component {
     movie: []
   }
 
+  // 외부에 async 를 쓰고 내부에 await을 쓰게되면 await을 붙은 부분의 답이 끝나기를 기다린다. -> axios 가 항상 빠르진 않기 떄문에 비동기 제어를 한 것
+  getMovies = async () => {
+    const movies = await axios.get("http://yts-proxy.now.sh/list_movies.json")
+  }
+
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 4000);
+    this.getMovies()
   }
 
   render() {
