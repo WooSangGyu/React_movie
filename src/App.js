5c0668cf9c2346bbe4214438f3272f61e4e1c9f9
woosanggyu <wsg0701@naver.com>
2021-04-12 13:39:21 +0900

    eslint delete and applied to gitignore

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Navigation from "./routes/Navigation";
+import "./App.css";
+
+function App() {
+  return <BrowserRouter>
+    <Navigation />
+    <Route path="/" exact={true} component={Home} />
+    <Route path="/about" component={About}/>
+  </BrowserRouter>
+}
+
+export default App;
